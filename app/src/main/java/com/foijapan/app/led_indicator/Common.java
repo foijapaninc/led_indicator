557e70b8c27413fe7291b1a0cc5d54df5546c979
Masahiko TAKAI <takai-m@zero-sum.co.jp>
2021-11-16 18:31:31 +0900

    no message

--- app/src/main/java/com/foijapan/app/led_indicator/Common.java
+++ app/src/main/java/com/foijapan/app/led_indicator/Common.java
@@ -6,6 +6,10 @@ public class Common {
     static String CHECKED_NUM = "CHECKED_NUM";
     static String CHECKED_APP = "CHECKED_APP_";
     static String PATTERN = "PATTERN";
+
+    static String DURATION_INFINITY = "DURATION_INFINITY";
+    static String DURATION = "DURATION";
+    static String INTERVAL = "INTERVAL";
     static int getNum(){
         return 41;
     }
--- app/src/main/java/com/foijapan/app/led_indicator/Pref.java
+++ app/src/main/java/com/foijapan/app/led_indicator/Pref.java
@@ -4,15 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class Pref extends Activity
+public class Pref extends Activity implements TextWatcher
 {
     private int mMin = 0;
     private int mSec = 0;
@@ -26,11 +29,28 @@ public class Pref extends Activity
 
         Intent intent = getIntent();
         final ArrayList<String> pns = intent.getStringArrayListExtra(Common.LIST_OF_CHECKED_APP);
+        final CheckBox cbInfinity = findViewById(R.id.id_cb_tn_infinity);
+        final EditText etMin = findViewById(R.id.id_et_min);
+        final EditText etSec = findViewById(R.id.id_et_sec);
+
+        etMin.addTextChangedListener(this);
+        etSec.addTextChangedListener(this);
+
+        cbInfinity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(cbInfinity.isChecked()){
+                    etMin.setText("");
+                }
+                etMin.setEnabled(!(cbInfinity.isChecked()));
+            }
+        });
 
         Button btnSettings = findViewById(R.id.id_btn_setting);
         btnSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String warning = "";
                 SharedPreferences pref = getSharedPreferences(Common.PREF,MODE_PRIVATE);
                 SharedPreferences.Editor edit = pref.edit();
                 edit.putInt(Common.CHECKED_NUM,pns.size());
@@ -39,16 +59,39 @@ public class Pref extends Activity
                     edit.putString(Common.CHECKED_APP+String.valueOf(i), pns.get(i));
                 }
 
-                int setMin = getMin();
+                int setMin = getMin(etMin);
                 if(setMin <= 0 || setMin > 99) {
-                    Toast.makeText(getApplicationContext(),getString(R.string.str_tst_err_min), Toast.LENGTH_SHORT).show();
+                    warning = String.format(getString(R.string.str_tst_err_pref),
+                                                    getString(R.string.str_time_notification1),
+                                                    getString(R.string.str_time_notification2));
                 }
 
-                int setSec = getSec();
-                if(setMin <= 0 || setMin > 99) {
-                    Toast.makeText(getApplicationContext(),getString(R.string.str_tst_err_sec), Toast.LENGTH_SHORT).show();
+                if(!(cbInfinity.isChecked()) && !warning.isEmpty()){
+                    Toast.makeText(getApplicationContext(),warning,Toast.LENGTH_SHORT).show();
+                    warning = "";
                 }
 
+                int setSec = getSec(etSec);
+                if(setSec <= 0 || setSec> 99) {
+                    warning = String.format(getString(R.string.str_tst_err_pref),
+                            getString(R.string.str_repeat_interval1),
+                            getString(R.string.str_repeat_interval2));
+                }
+
+                if(!warning.isEmpty()){
+                    Toast.makeText(getApplicationContext(),warning,Toast.LENGTH_SHORT).show();
+                }
+
+                initialize(edit);
+
+                edit.putBoolean(Common.DURATION_INFINITY,cbInfinity.isChecked());
+                if(cbInfinity.isChecked()){
+                    edit.putInt(Common.DURATION, 0);
+                }
+                else {
+                    edit.putInt(Common.DURATION,Integer.parseInt(etMin.getText().toString()));
+                }
+                edit.putInt(Common.INTERVAL,Integer.parseInt(etSec.getText().toString()));
                 edit.apply();
             }
         });
@@ -57,25 +100,45 @@ public class Pref extends Activity
         btnPreview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Flash flash = new Flash(getApplicationContext());
-                flash.blink1();
+                flash.blink2();
             }
         });
     }
 
-    private int getMin(){
-        EditText etMin = findViewById(R.id.id_et_min);
+    private void initialize(SharedPreferences.Editor editor){
+        editor.putBoolean(Common.DURATION_INFINITY,false);
+        editor.putInt(Common.DURATION, 0);
+        editor.putInt(Common.INTERVAL,0);
+    }
+
+    private int getMin(EditText etMin){
         if(etMin.getText().toString().equals(getString(R.string.tn_min))){
             return 0;
         }
         return Integer.parseInt(etMin.getText().toString());
     }
 
-    private int getSec(){
-        EditText etSec = findViewById(R.id.id_et_sec);
+    private int getSec(EditText etSec){
         if(etSec.getText().toString().equals(getString(R.string.tn_sec))){
             return 0;
         }
         return Integer.parseInt(etSec.getText().toString());
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
     }
 }
